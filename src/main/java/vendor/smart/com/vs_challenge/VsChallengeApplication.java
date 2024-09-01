a89d8a30897b4367afb7e8cdde2b76ce01703beb
Cézar Augusto <cezar.silva@zenklub.com>
2024-09-01 19:34:46 -0300

    feat: first step

---
+++ src/main/java/vendor/smart/com/vs_challenge/VsChallengeApplication.java
@@ -0,0 +1,12 @@
+package vendor.smart.com.vs_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VsChallengeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(VsChallengeApplication.class, args);
+  }
+}
